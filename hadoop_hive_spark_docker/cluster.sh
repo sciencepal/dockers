3f2145eebb4bee8a9b29b4e6e158d447fe685ae5
sciencepal <aditya.pal.science@gmail.com>
2019-02-26 23:02:10 +0530

    Spark Added

--- hadoop_hive_spark_docker/cluster.sh
+++ hadoop_hive_spark_docker/cluster.sh
@@ -10,18 +10,30 @@ function startServices {
   echo ">> Starting yarn ..."
   docker exec -u hadoop -d nodemaster /home/hadoop/hadoop/sbin/start-yarn.sh
   sleep 5
-  echo ">>Starting MR-JobHistory Server ..."
+  echo ">> Starting MR-JobHistory Server ..."
   docker exec -u hadoop -d nodemaster /home/hadoop/hadoop/sbin/mr-jobhistory-daemon.sh start historyserver
   sleep 5
-  echo ">>Starting Spark ..."
-  docker exec -u hadoop -d nodemaster /home/hadoop/sparkcmd.sh start
-  docker exec -u hadoop -d node2 /home/hadoop/sparkcmd.sh start
-  docker exec -u hadoop -d node3 /home/hadoop/sparkcmd.sh start
+  echo ">> Starting Spark ..."
+  docker exec -u hadoop -d nodemaster /home/hadoop/spark/sbin/start-master.sh
+  docker exec -u hadoop -d node2 /home/hadoop/spark/sbin/start-slave.sh nodemaster:7077
+  docker exec -u hadoop -d node3 /home/hadoop/spark/sbin/start-slave.sh nodemaster:7077
+  sleep 5
+  echo ">> Starting Spark History Server ..."
+  docker exec -u hadoop nodemaster /home/hadoop/spark/sbin/start-history-server.sh
   echo "Hadoop info @ nodemaster: http://172.18.1.1:8088/cluster"
   echo "DFS Health @ nodemaster : http://172.18.1.1:50070/dfshealth"
   echo "MR-JobHistory Server @ nodemaster : http://172.18.1.1:19888"
-  echo "Secondary Namenode @ nodemaster : http://172.18.1.1:50090"
   echo "Spark info @ nodemaster  : http://172.18.1.1:8080"
+  echo "Spark History Server @ nodemaster : http://172.18.1.1:18080"
+}
+
+function stopServices {
+  echo ">> Stopping Spark Master and slaves ..."
+  docker exec -u hadoop -d nodemaster /home/hadoop/spark/sbin/stop-master.sh
+  docker exec -u hadoop -d node2 /home/hadoop/spark/sbin/stop-slave.sh
+  docker exec -u hadoop -d node3 /home/hadoop/spark/sbin/stop-slave.sh
+  echo ">> Stopping containers ..."
+  docker stop nodemaster node2 node3
 }
 
 if [[ $1 = "start" ]]; then
@@ -30,12 +42,7 @@ if [[ $1 = "start" ]]; then
 fi
 
 if [[ $1 = "stop" ]]; then
-  docker exec -u hadoop -d nodemaster /home/hadoop/sparkcmd.sh stop
-  docker exec -u hadoop -d node2 /home/hadoop/sparkcmd.sh stop
-  docker exec -u hadoop -d node3 /home/hadoop/sparkcmd.sh stop
-  docker stop nodemaster node2 node3
-  docker rm nodemaster node2 node3
-  docker system prune -f
+  stopServices
   exit
 fi
 
@@ -57,7 +64,25 @@ if [[ $1 = "deploy" ]]; then
   exit
 fi
 
-echo "Usage: cluster.sh deploy|start|stop"
+
+if [[ $1 = "undeploy" ]]; then
+  stopServices
+  docker rm nodemaster node2 node3
+  docker network rm hadoopnet
+  exit
+fi
+
+
+if [[ $1 = "uninstall" ]]; then
+  stopServices
+  docker rmi hadoop hadoopbase spark hadoop_spark_hive -f
+  docker system prune -f
+  exit
+fi
+
+echo "Usage: cluster.sh deploy|start|stop|undeploy|uninstall"
 echo "                 deploy - create a new Docker network"
 echo "                 start  - start the existing containers"
-echo "                 stop   - stop the running containers"  
+echo "                 stop   - stop the running processes"
+echo "                 undeploy - stop running containers and remove them"
+echo "                 uninstall - remove all docker images"
