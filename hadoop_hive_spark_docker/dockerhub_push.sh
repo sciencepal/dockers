35dee955878a6c4748b63dff284f0dc76fb6dead
sciencepal <aditya.pal.science@gmail.com>
2020-06-15 14:53:37 +0530

    Added java env variable in nifi-env.sh

---
+++ hadoop_hive_spark_docker/dockerhub_push.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# push to dockerhub
+
+# Hadoop
+docker push sciencepal/hadoop_cluster:hadoop
+
+# Spark
+docker push sciencepal/hadoop_cluster:spark
+
+# PostgreSQL Hive Metastore Server
+docker push sciencepal/hadoop_cluster:postgresql-hms
+
+# Hive
+docker push sciencepal/hadoop_cluster:hive
+
+# Nifi
+docker push sciencepal/hadoop_cluster:nifi
+
+# Edge
+docker push sciencepal/hadoop_cluster:edge
+
+# hue
+docker push sciencepal/hadoop_cluster:hue
+
+# zeppelin
+docker push sciencepal/hadoop_cluster:zeppelin
